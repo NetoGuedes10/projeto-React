cbc70e5411bdbf651b39b05b3345863f9c0fcf7a
NetoGuedes10 <gilzethfernandes@gmail.com>
2023-09-30 11:52:25 -0300

    meu primerio react

---
+++ src/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+import Backgroud from "./Assets/backgrond1.svg"
+
+export const Container = styled.div`
+   background: url("${Backgroud}");
+   background-size: cover;
+`;
+
+export const image = styled.img`
+`;
+
+export const ContainerItens = styled.div`
+`;
+
+export const H1 = styled.h1`
+`
+
+export const InputLabel = styled.p`
+`
+
+export const Input = styled.input`
+`;
+
+export const Button = styled.button`
+`;
